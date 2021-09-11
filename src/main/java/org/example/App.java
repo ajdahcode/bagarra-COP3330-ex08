8dc2a60af2f4ce80ddb7a2216d0b42cf890668e9
ajdahcode <90115717+ajdahcode@users.noreply.github.com>
2021-09-11 00:28:04 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,14 +2,7 @@
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 alden bagarra
  */
-/*
-How many people? 8
-How many pizzas do you have? 2
-How many slices per pizza? 8
-8 people with 2 pizzas (16 slices)
-Each person gets 2 pieces of pizza.
-There are 0 leftover pieces.
- */
+
 package org.example;
 import java.util.Scanner;
 
